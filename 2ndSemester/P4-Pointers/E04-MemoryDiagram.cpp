97f481c787cbe187bc3322b164ed281b820cb492
Ray <reinamlaura@gmail.com>
2025-02-04 17:00:46 -0400

    feat: added execution analysis to P4-E04

--- 2ndSemester/P4-Pointers/E04-MemoryDiagram.cpp
+++ 2ndSemester/P4-Pointers/E04-MemoryDiagram.cpp
@@ -10,17 +10,17 @@ struct node {
 };
 
 int main ( ) { 
-    node *p, *r, *s; 
+    node *p, *r, *s; //Se crean tres punteros a nodos, p, r y s
     p = new node(); 
-    s = new node(); 
+    s = new node(); //Se le asignan a p, r y s un puntero para cada uno
     r = new node(); 
-    p->next = r; 
-    r->next = s; 
-    s->next = p; 
-    s->info = 3; 
-    p->next->next->next->info = 2; 
-    s->next->next->info = 1; 
-    p = s->next; 
-    std::cout << p->info << " " << s->info << " " << r->info << std::endl; 
+    p->next = r; //Se le asigna la direccion de r a puntero next del nodo al que p apunta
+    r->next = s; //Se le asigna la direccion de s a puntero next del nodo al que r apunta
+    s->next = p; //Se le asigna la direccion de p a puntero next del nodo al que s apunta
+    s->info = 3; //Se le asigna el valor 3 al entero info del nodo al que s apunta
+    p->next->next->next->info = 2; //Se le asigna el valor 2 al entero info del nodo al que p apunta
+    s->next->next->info = 1; //Se le asigna el valor 1 al entero info del nodo al que r apunta
+    p = s->next; //Se vuelve a asignar al puntero p la direccion del nodo al que ya apuntaba anteriormente
+    std::cout << p->info << " " << s->info << " " << r->info << std::endl;  //Se imprime "2 3 1"
     return 0; 
 }
